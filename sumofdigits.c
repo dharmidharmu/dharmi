cf94ae20dbaea54e27e4eda85818f108b0c9c2ab
dharmidharmu <42268505+dharmidharmu@users.noreply.github.com>
2018-08-28 19:33:12 +0530

    Add files via upload

---
+++ sumofdigits.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,temp,sum=0;
+
+    printf("enter the number\n");
+    scanf("%d",&a);
+    while(a!=0)
+    {
+        temp=a%10;
+        sum+=temp;
+        a=a/10;
+    }
+    printf("sum of digits is %d",sum);
+
+}
